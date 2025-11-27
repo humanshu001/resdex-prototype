73f22bda2a4d15b98b8984f839e3b0d1265b5d20
Humanshu Jaglan <humanshujaglan@gmail.com>
2025-11-27 11:50:58 +0530

    hmm

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,39 +1,14 @@
 import { PrismaClient } from "@prisma/client";
-import { PrismaPg } from "@prisma/adapter-pg";
-// @ts-ignore
-import pg from "pg";
 
-declare global {
-  var prisma: PrismaClient | undefined;
-}
-
-const pool = new pg.Pool({
-  connectionString: process.env.DATABASE_URL,
-  ssl: process.env.NODE_ENV === "production" ? { rejectUnauthorized: false } : false,
-
-  // ⭐ THIS IS THE FIX:
-  // pg.Pool does NOT apply search_path unless set in "connection parameters"
-  // so we must use this:
-  // key: "options", value: "-c search_path=resdex"
-  // but it must be passed as an environment variable like below ↓↓↓
-});
-
-// ⭐ After pool connect, force search_path:
-pool.on("connect", (client:any) => {
-  client.query(`SET search_path TO public;`);
-});
-
-const adapter = new PrismaPg(pool);
+// Prevent multiple instances of Prisma Client in development
+const globalForPrisma = global as unknown as { prisma: PrismaClient };
 
 export const prisma =
-  global.prisma ||
+  globalForPrisma.prisma ||
   new PrismaClient({
-    adapter,
     log: ["query"],
   });
 
-if (process.env.NODE_ENV !== "production") {
-  global.prisma = prisma;
-}
+if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
 
 export default prisma;
