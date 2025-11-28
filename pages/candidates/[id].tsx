1f3613a0e34963aa28f76623eb846074a6289874
Humanshu Jaglan <140067860+humanshu001@users.noreply.github.com>
2025-11-28 15:16:31 +0530

    done

--- pages/candidates/[id].tsx
+++ pages/candidates/[id].tsx
@@ -86,7 +86,7 @@ export default function CandidateProfilePage({ candidate }: Props) {
         <CardBody className="py-6 flex flex-col items-start text-center -mb-4">
           <div className="w-full pl-3 z-10">
             <div className="w-20 h-20 rounded-2xl bg-gradient-to-br from-gray-700 to-black text-white flex items-center justify-start text-3xl font-bold mt-5 z-10 border-4 border-white">
-            <div className="rounded-xl w-full h-full bg-gradient-to-r from-yellow-200 to-white">
+            <div className="rounded-xl w-full h-full bg-gradient-to-r from-yellow-200 to-white flex justify-center items-center text-black">
               {candidate.full_name?.charAt(0).toUpperCase()}
             </div>
           </div>
